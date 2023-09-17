a66f1ae766adc2b45b73afb57934509f6de778e1
Dritriy Golyshev <4dimariK@gmail.com>
2023-09-17 07:49:03 +0300

    feat(view): add blade layout, components

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('shared.header')
+    <main class="py-16 lg:py-20">
+        <div class="container">
+            <h1 class="text-[26px] sm:text-xl xl:text-[48px] 2xl:text-2xl font-black">
+                Лучшие статьи
+            </h1>
+            <div class="tasks grid gap-4 grid-cols-1 lg:grid-cols-2 gap-x-10 gap-y-14 xl:gap-y-20 mt-12 md:mt-20">
+                @include('shared.post')
+                @include('shared.post')
+                @include('shared.post')
+                @include('shared.post')
+            </div>
+        </div>
+    </main>
+    @include('shared.footer')
+@endsection
